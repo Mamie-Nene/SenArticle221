c439de4cce645d17bd3607fa0af6d60c86b5463c
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-04 18:01:25 +0000

    Update connexion.php

--- views/connexion.php
+++ views/connexion.php
@@ -6,25 +6,26 @@
 
 <head>
     <title> la page de connexion</title>
-    
+    <link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 
 <body>
     <form action="../models/connexion.php" method="POST">
         <h2> Connexion </h2>
-        <div class="form-group row">
+        <div class="input-group">
             <label for="name"> username : </label>
             <input type="text" name="uname" />
             <br>
         </div>
-        <div class="form-group row">
+        <div class="input-group">
             <label for="name">Mot de passe : </label>
             <input type="password"  name="mdp" />
             <br>
+        </div>
+        <div class="input-group">
+            <button class="btn" type="submit" name="connexion"> Connexion </button>
 
-            <button type="submit" name="connexion"> Connexion </button>
-
-            <a href="inscription.php"> <button type="button" name="inscription"> Inscription</button></a>
+             <button class="btn" type="button" name="inscription"> <a href="inscription.php"> Inscription </a> </button>
         </div>
     </form>
 </body>
