bcd97e6cce517415605b49202f3b6ea3372283eb
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-05 02:28:35 +0000

    Update indexEditeurs.php

--- views/indexEditeurs.php
+++ views/indexEditeurs.php
@@ -1,32 +1,11 @@
 <?php
+        require_once "../models/databaseConnexion.php";
         require '../controllers/ControllerEditeurs.php';  
-        $controller = new Controller();
+        $controller = new ControllerAdmin();
         //s'il nya pas d'actions
         if(!isset($_GET['action'])){
             $controller->showAccueil();
-            $controller->showCrudArticle();
-            $controller->showCrudCategorie();
         }
-        else 
-        {
-            if($_GET['action']==='articles')
-            {
-                if(isset($_GET['id']))
-                {
-                    $controller->showArticles();
-                }
-            }
-            else
-            {   
-                if($_GET['action']==='categories')
-                {
-                    if(isset($_GET['id']))
-                    {
-                        $controller->showCategories();
-                    }
-                }
-                else $controller->showAccueil();
-            } 
-        } 
+        
 ?>
        
