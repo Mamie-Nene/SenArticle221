0ac07504cdbe9ac14400ccbd3a0fad573d541f8c
MAAC <49979494+MohammadAbdoulAzizCiss@users.noreply.github.com>
2021-11-05 20:10:41 +0000

    Update addArticle.php

--- models/addArticle.php
+++ models/addArticle.php
@@ -5,7 +5,7 @@ session_start();
             echo 'veuillez remplir tous les champs ';
         }
         else{
-            $connexion =mysqli_connect("127.0.0.1", "221shadow", "772972920lodye", "mglsi_news") ;
+            $connexion =mysqli_connect("127.0.0.1", "221shadow", "mot de passe", "mglsi_news") ;
             $titre= $_POST['titre'];
             $contenu= $_POST['contenu'];
             $editeurs= $_SESSION['username']; 
