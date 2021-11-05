b2573d008f4779015590e30923f7f5cf4422b36e
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-05 02:41:47 +0000

    Update ControllerAdmin.php

--- controllers/ControllerAdmin.php
+++ controllers/ControllerAdmin.php
@@ -1,32 +1,17 @@
 <?php
-    require_once '../models/mesFonctionsSql.php';
-    class Controller{
+    require_once '../models/Article.php';
+    require_once '../models/Categorie.php';
+    class ControllerAdmin{
         function __construct()
         {
         }
-        public function showAccueil(){
-            ?>
-            <!DOCTYPE html>
-            <html lang="FR">
-    <head>
-        <title>page d'accueil </title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    </head>
-    <body>
-         <h2 class="text-center"> Bienvenue à SenArticle221 </h2>
-         <button> <a href="../models/logout.php"> Deconnexion</a> </button>
-         <hr>
-       
-    </body>
-</html>
-            <?php
-            require_once '../views/article.php';
-             afficherArticle();
-             afficherCategorie();
+        public function showAccueil()
+        {
+        require_once '../views/accueilAdmin.php';
         }
         public function showArticles(){
             
-            readArticle($articles);
+            
         }
         public function showCategories(){
             $categories = getAllCategory();
