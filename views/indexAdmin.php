97a2ce3bbaa2ac81ee33713f714f960adcf1a8fa
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-05 02:29:20 +0000

    Update indexAdmin.php

--- views/indexAdmin.php
+++ views/indexAdmin.php
@@ -1,33 +1,13 @@
 <?php
+        require_once "../models/databaseConnexion.php";
         require '../controllers/ControllerAdmin.php';  
-        $controller = new Controller();
+        $controller = new ControllerAdmin();
         //s'il nya pas d'actions
         if(!isset($_GET['action'])){
             $controller->showAccueil();
-            $controller->showCrudArticle();
-            $controller->showCrudCategorie();
-            $controller->showCrudUser();
+           // $controller->showCrudArticle();
+            //$controller->showCrudCategorie();
+            //$controller->showCrudUser();
         }
-        else 
-        {
-            if($_GET['action']==='articles')
-            {
-                if(isset($_GET['id']))
-                {
-                    $controller->showArticles();
-                }
-            }
-            else
-            {   
-                if($_GET['action']==='categories')
-                {
-                    if(isset($_GET['id']))
-                    {
-                        $controller->showCategories();
-                    }
-                }
-                else $controller->showAccueil();
-            } 
-        } 
 ?>
        
