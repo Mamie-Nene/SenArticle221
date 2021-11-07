5cf85cad4bcf85949445df545872a620f2e299ea
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 23:59:17 +0000

    Update indexEditeurs.php

--- views/indexEditeurs.php
+++ views/indexEditeurs.php
@@ -1,13 +1,74 @@
 <?php
-        session_start();    
-        var_dump($_SESSION['username']);
-        require_once "../models/databaseConnexion.php";
-        require '../controllers/ControllerEditeurs.php';  
+    session_start();     
+        require_once '../controllers/ControllerEditeur.php';  
         $controller = new ControllerAdmin();
         //s'il nya pas d'actions
-        if(!isset($_GET['action'])){
+        if(!isset($_GET['action']))
+        {
             $controller->showAccueil();
         }
-        
-?>
+        else
+        {
+            if(strtolower($_GET['action'])==='article')
+            {
+                if(isset($_GET['id']))
+                {
+                    $controller->showArticles($_GET['id']);
+                }
+                else
+                {
+                    echo'Erreur';
+                }
+            }
+            elseif(strtolower($_GET['action'])==='categorie')
+            {   
+                if(isset($_GET['id']))
+                {
+                    $controller->showCategories($_GET['id']);
+                }
+                else
+                {
+                    echo'Erreur';
+                }
+            }
+            elseif(strtolower($_GET['action'])==='gererarticles')
+            {   
+                    $controller->showGestionArticle();
+            }
+            elseif(strtolower($_GET['action'])==='editarticle')
+            {   
+                if(isset($_GET['id']))
+                {
+                    $controller->showEditArticle($_GET['id']);
+                }
+                else
+                {
+                    echo'Erreur';
+                }
+            }
+            elseif(strtolower($_GET['action'])==='deletearticle')
+            {   
+                if(isset($_GET['id']))
+                {
+                    $controller->showDeleteArticle($_GET['id']);
+                }
+                else
+                {
+                    echo'Erreur';
+                }
+            }
+            elseif(strtolower($_GET['action'])==='gerercategories')
+            {   
+                    $controller->showGestionCategorie();
+            }
+            elseif(strtolower($_GET['action'])==='gererusers')
+            {   
+                    $controller->showGestionUsers();
+            }
+            elseif (strtolower($_GET['action'])==='pagination')
+            {
+                $controller->showPagination(); 
+            }
+            else $controller->showAccueil();
+        }
        
