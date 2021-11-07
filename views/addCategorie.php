d7b17b352804b392ab2503fd9c029ecd2914611f
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 01:23:40 +0000

    Update addCategorie.php

--- views/addCategorie.php
+++ views/addCategorie.php
@@ -6,14 +6,14 @@
     </head>
     <body>
         <h1> Formulaire d'ajout d'une catégorie </h1>
-        <form action="../models/addArticle.php" method="post">
+        <form action="../models/addCategorie.php" method="post">
             <div class="input-group">
                 <label for="libelle"> Libellé </label> 
                 <input type="textarea" name="libelle" id="libelle"/> 
             </div>
             <div class="input-group">
-                <button type="submit" value="valider" class="btn">VALIDER </button>
-                <a href="indexEditeurs.php"> <button class="btn" type="button"> Annuler </button> </a>
+                <button type="submit" name="valider" class="btn">VALIDER </button>
+                <a href="../views/indexAdmin.php?action=gerercategories">  <button class="btn" type="button"> Annuler </button> </a>
             </div>
         </form>
     </body>
