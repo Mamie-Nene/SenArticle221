1d73acb924aa2de7cedcccc344a2e48685b921dc
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 01:30:48 +0000

    Update addArticle.php

--- models/addArticle.php
+++ models/addArticle.php
@@ -1,36 +1,26 @@
 <?php
-    if(isset($_POST['valider']))
-    {      
-        if((empty($_POST['titre'])) or (empty($_POST['contenu'])) or (empty($_POST['dateCreation'])) or (empty($_POST['dateModif'])))
-        {
+session_start();
+    if(isset($_POST['valider'])){      
+        if((empty($_POST['titre'])) or (empty($_POST['contenu'])) or (empty($_POST['categorie']))){
             echo 'veuillez remplir tous les champs ';
         }
-        else
-        {
-            $titre= mysqli_real_escape_string($connexion,$_POST['titre']);
-            $contenu= mysqli_real_escape_string($connexion,$_POST['contenu']);
-            $dateCreation= mysqli_real_escape_string($connexion,$_POST['dateCreation']);
-            $dateModif= mysqli_real_escape_string($connexion,$_POST['dateModif']);
-            $editeurs= mysqli_real_escape_string($connexion,$_POST['editeurs']);
-                            
-            $requete ="INSERT INTO Users (nom, username, motDePasse, mail, groupe) VALUES ('".$nom."', '".$uname."', '".$mdp."', '".$mail."', '".$groupe."')";
-            $req = mysqli_query($connexion,$requete);
-            //retunr true si c'est reussi
-            if($req)
-            {
-            echo 'inscription avec succes';
-            header("Location: ../indexEditeurs.php");
-            }
-            else
-            {
-                echo 'probleme bd';
-            }
-                    
-        }
-            //hachage du mot de passe 
-            //$pass_hache=password_hash($_POST['mdp'], PASSWORD_DEFAULT);
-            //pour eviter les attaques de type injection sql et xss
-            // on insere les données dans la bd
-                        
+        else{
+            $connexion =mysqli_connect("127.0.0.1", "root", "passer123", "mglsi_news") ;
+            //$connexion=databaseConnexion::getDatabaseConnexion();
+            $titre= $_POST['titre'];
+            $contenu= $_POST['contenu'];
+            $editeur= $_SESSION['id']; 
+            $categorie = intval($_POST['categorie']);
+            $requete ="INSERT INTO Article (titre, contenu, categorie, editeur) VALUES ('{$titre}','{$contenu}',{$categorie},'{$editeur}')";
+            if (mysqli_query($connexion, $requete)) {
+                header("Location: ../views/indexAdmin.php?action=gererarticles");
+                
+              } else {
+                    echo "couldnt add article";
+            }       
+        }         
+    }else{
+        echo "didnt go in the if statement";
     }
 ?>
+    
