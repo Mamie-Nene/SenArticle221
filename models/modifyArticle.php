53d07f6f01efd953b1ac1e6dbca89776f91056ee
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 14:03:15 +0000

    Add files via upload

---
+++ models/modifyArticle.php
@@ -0,0 +1,25 @@
+<?
+session_start();
+if(isset($_POST['modifier'])){      
+        if((empty($_POST['titre'])) or (empty($_POST['contenu'])) or (empty($_POST['categorie']))){
+            echo 'veuillez remplir tous les champs ';
+        }
+        else{
+            $connexion =mysqli_connect("127.0.0.1", "root", "passer123", "mglsi_news") ;
+            //$connexion=databaseConnexion::getDatabaseConnexion();
+            $titre= $_POST['titre'];
+            $contenu= $_POST['contenu'];
+            $editeur= $_SESSION['id']; 
+            $categorie = intval($_POST['categorie']);
+            $requete ="UPDATE Article SET titre= '{$titre}', contenu= '{$contenu}',categorie={$categorie} ,editeur= '{$editeur}' where id='{$id}'";
+            if (mysqli_query($connexion, $requete)) {
+                header("Location: ../views/indexAdmin.php?action=gererarticles");
+                
+              } else {
+                    echo "couldnt add article";
+            }       
+        }         
+    }else{
+        echo "didnt go in the if modif statement";
+    }
+    ?>
