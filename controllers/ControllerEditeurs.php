127b746e7cc2bd33a3c9a856c3fa9abcdef42df5
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 23:47:06 +0000

    Update ControllerEditeurs.php

--- controllers/ControllerEditeurs.php
+++ controllers/ControllerEditeurs.php
@@ -2,16 +2,49 @@
     require_once "../models/databaseConnexion.php";
     require_once '../models/Article.php';
     require_once '../models/Categorie.php';
-    class ControllerAdmin
-    {
+    class ControllerAdmin{
         function __construct()
         {
         }
         public function showAccueil()
         {
-             $articles = Article::getAllArticle();
+            $articles = Article::getAllArticle();
             $categories= Categorie::getAllCategory();
-        require_once '../views/accueilEditeurs.php';
+        require_once '../views/accueilEditeur.php';
         }
-    }
+        public function showArticles($id){
+            
+            $article=Article::ArticleById($id);
+            $categories= Categorie::getAllCategory();
+            require '../views/articleEditeur.php';
+            
+        }
+        public function showCategories($id){
+            $articles=Article::categoryArticle($id);
+            $categories=Categorie::getAllCategory();
+            require '../views/categorieEditeur.php';
+        }
+        public function showGestionArticle(){
+            $articles=Article::getAllArticle();
+            require '../views/gererArticlesEditeurs.php';
+        }
+         public function showEditArticle($id){
+            
+            $article=Article::ArticleById($id);
+            require '../views/editArticle.php';
+            
+        }
+        public function showDeleteArticle($id){
+            
+            $article=Article::ArticleById($id);
+            require '../views/deleteArticle.php';
+            
+        }
+        public function showGestionCategorie(){
+            $categories=Categorie::getAllCategory();
+            require '../views/gererCategoriesEditeurs.php';
+        }
+        
+    } 
+
 ?>
