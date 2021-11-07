37967bc0ffcd8d15a08f7f1ef434b37a8e99b05b
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 01:28:13 +0000

    Update indexAdmin.php

--- views/indexAdmin.php
+++ views/indexAdmin.php
@@ -1,15 +1,52 @@
 <?php
-        session_start();    
-         
-        require_once "../models/databaseConnexion.php";
-        require '../controllers/ControllerAdmin.php';  
+    session_start();     
+        require_once '../controllers/ControllerAdmin.php';  
         $controller = new ControllerAdmin();
         //s'il nya pas d'actions
-        if(!isset($_GET['action'])){
+        if(!isset($_GET['action']))
+        {
             $controller->showAccueil();
-            $controller->showCrudArticle();
-            $controller->showCrudCategorie();
-            $controller->showCrudUser();
         }
-?>
+        else
+        {
+            if(strtolower($_GET['action'])==='article')
+            {
+                if(isset($_GET['id']))
+                {
+                    $controller->showArticles($_GET['id']);
+                }
+                else
+                {
+                    echo'Erreur';
+                }
+            }
+            elseif(strtolower($_GET['action'])==='categorie')
+            {   
+                if(isset($_GET['id']))
+                {
+                    $controller->showCategories($_GET['id']);
+                }
+                else
+                {
+                    echo'Erreur';
+                }
+            }
+            elseif(strtolower($_GET['action'])==='gererarticles')
+            {   
+                    $controller->showGestionArticle();
+            }
+            elseif(strtolower($_GET['action'])==='gerercategories')
+            {   
+                    $controller->showGestionCategorie();
+            }
+            elseif(strtolower($_GET['action'])==='gererusers')
+            {   
+                    $controller->showGestionUsers();
+            }
+            elseif (strtolower($_GET['action'])==='pagination')
+            {
+                $controller->showPagination(); 
+            }
+            else $controller->showAccueil();
+        }
        
