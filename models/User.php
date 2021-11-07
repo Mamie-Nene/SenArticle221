24913e57374566b91ede6617cd32168f323fdc52
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-07 23:50:00 +0000

    Update User.php

--- models/User.php
+++ models/User.php
@@ -1,22 +1,19 @@
 <?php
 class User{
-    public static function getAllUsers(){ 
+    public static function getAllUsers()
+    { 
         $connexion=databaseConnexion::getDatabaseConnexion();
-        //on met la requete oour récuperer tous les articles 
         $requete='SELECT * FROM Users';
-        // et pour le la fonction query c'est pour executer (preparer) la requete
         $donnees =$connexion->query($requete);
        //affiche en ligne (rows) et stockage du resultat dans un tableau associatif
         $rows =$donnees->fetchAll();
         return $rows;
     }
-    public static function getAllGroupe(){ 
+    public static function getAllGroupe()
+    { 
         $connexion=databaseConnexion::getDatabaseConnexion();
-        //on met la requete oour récuperer tous les articles 
         $requete='SELECT * FROM Groupe';
-        // et pour le la fonction query c'est pour executer (preparer) la requete
         $donnees =$connexion->query($requete);
-       //affiche en ligne (rows) et stockage du resultat dans un tableau associatif
         $rows =$donnees->fetchAll();
         return $rows;
     }
