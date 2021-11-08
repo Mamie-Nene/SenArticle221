08e395663018288084c89c5dd311e24d9bec8b40
Mame Néné BA <52071239+Mamie-Nene@users.noreply.github.com>
2021-11-08 00:00:18 +0000

    Update connexion.php

--- views/connexion.php
+++ views/connexion.php
@@ -6,38 +6,27 @@
 
 <head>
     <title> la page de connexion</title>
-    
-    <!-- Bootstrap core CSS -->
-  <link href="../asset/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!--external css-->
-  <link href="../asset/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
-  <!-- Custom styles for this template -->
-  <link href="../asset/css/style.css" rel="stylesheet">
-  <link href="../asset/css/style-responsive.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 
 <body>
-    <div id="login-page">
-        <div class="container">
-            <form class="form-login" action="../models/connexion.php" method="POST">
-                <h2 class="form-login-heading"> Connectez-vous </h2>
-                <div class="login-wrap"> 
-                    <input class="form-control" type="text" name="uname" placeholder="Nom d'utilisateur"/>
-                    <br/>
-                    <input class="form-control" type="password"  name="mdp" placeholder="Mot de passe"/> <br/>
-                    <button class="btn btn-theme btn-block" type="submit" name="connexion"><i class="fa fa-lock"></i> Connexion </button>
-                    <hr>
-                    <div class="registration">
-                        Vous n'avez pas encore de compte? <br/>
-                        <a href="inscription.php">   Inscrivez vous!  </a> 
-                    </div>
-                </div>
-            </form>
+    <form action="../models/connexion.php" method="POST">
+        <h2> Connexion </h2>
+        <div class="input-group">
+            <input type="text" name="uname" placeholder="Nom d'utilisateur"/>
+            <br>
         </div>
-    </div>
+        <div class="input-group">
+            <br>
+            <input type="password"  name="mdp" placeholder="Mot de passe"/>
+            <br>
+        </div>
+        <div class="input-group">
+            <button class="btn" type="submit" name="connexion"> Connexion </button>
 
-   
-  
+            <a href="inscription.php"> <button class="btn" type="button" name="inscription">  Inscription </button> </a> 
+        </div>
+    </form>
 </body>
 
 </html>
